a2937fab8ea0fc37ed089aa4634d6ef35256b852
SHEORAN022 <141564857+SHEORAN022@users.noreply.github.com>
2026-01-07 01:39:25 +0530

    Create publicProducts.js

---
+++ routes/publicProducts.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const router = express.Router();
+const controller = require("../controllers/publicProductController");
+
+// 🌍 WEBSITE PUBLIC API
+router.get("/products", controller.getAllWebsiteProducts);
+
+module.exports = router;
