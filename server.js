814e4d48ac20913ba949506b3e7c1b61dfd1d3bc
SHEORAN022 <141564857+SHEORAN022@users.noreply.github.com>
2026-01-07 12:42:40 +0530

    Update server.js

--- server.js
+++ server.js
@@ -34,6 +34,7 @@ app.use(
   cors({
     origin: ["*",
       "http://localhost:3000",
+             "http://localhost:3001",
       "http://localhost:5173",
       "http://localhost:5174",
       "http://localhost:5175",
