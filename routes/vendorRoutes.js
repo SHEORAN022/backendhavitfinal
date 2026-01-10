612fdae35985cef44f7655654f2669d7001a26d8
SHEORAN022 <141564857+SHEORAN022@users.noreply.github.com>
2026-01-10 12:03:42 +0530

    Update vendorRoutes.js

--- routes/vendorRoutes.js
+++ routes/vendorRoutes.js
@@ -1,18 +1,56 @@
 
+// const express = require("express");
+// const router = express.Router();
+
+// const vendorAuth = require("../middleware/vendorAuth");
+// const productCtrl = require("../controllers/vendorProductController");
+// const subCtrl = require("../controllers/vendorSubCategoryController");
+// const catCtrl = require("../controllers/vendorCategoryController");
+
+// router.get("/categories", vendorAuth, catCtrl.getVendorCategories);
+// router.get("/subcategories", vendorAuth, subCtrl.getVendorSubCategories);
+
+// router.get("/products", vendorAuth, productCtrl.getVendorProducts);
+// router.post("/products", vendorAuth, productCtrl.createVendorProduct);
+// router.put("/products/:id", vendorAuth, productCtrl.updateVendorProduct);
+// router.delete("/products/:id", vendorAuth, productCtrl.deleteVendorProduct);
+
+// module.exports = router;
+
 const express = require("express");
 const router = express.Router();
-
 const vendorAuth = require("../middleware/vendorAuth");
-const productCtrl = require("../controllers/vendorProductController");
-const subCtrl = require("../controllers/vendorSubCategoryController");
-const catCtrl = require("../controllers/vendorCategoryController");
+const upload = require("../middleware/upload");
+const controller = require("../controllers/vendorProductController");
 
-router.get("/categories", vendorAuth, catCtrl.getVendorCategories);
-router.get("/subcategories", vendorAuth, subCtrl.getVendorSubCategories);
+router.use(vendorAuth);
 
-router.get("/products", vendorAuth, productCtrl.getVendorProducts);
-router.post("/products", vendorAuth, productCtrl.createVendorProduct);
-router.put("/products/:id", vendorAuth, productCtrl.updateVendorProduct);
-router.delete("/products/:id", vendorAuth, productCtrl.deleteVendorProduct);
+/* GET */
+router.get("/", controller.getVendorProducts);
+
+/* CREATE */
+router.post(
+  "/",
+  upload.fields([
+    { name: "image", maxCount: 1 },
+    { name: "logo", maxCount: 1 },
+    { name: "gallery", maxCount: 10 },
+  ]),
+  controller.createVendorProduct
+);
+
+/* UPDATE */
+router.put(
+  "/:id",
+  upload.fields([
+    { name: "image", maxCount: 1 },
+    { name: "logo", maxCount: 1 },
+    { name: "gallery", maxCount: 10 },
+  ]),
+  controller.updateVendorProduct
+);
+
+/* DELETE */
+router.delete("/:id", controller.deleteVendorProduct);
 
 module.exports = router;
