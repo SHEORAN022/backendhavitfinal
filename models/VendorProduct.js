d8ac98a0eafd3b9d2fd721d8837f0c978589e47c
SHEORAN022 <141564857+SHEORAN022@users.noreply.github.com>
2026-01-10 12:11:27 +0530

    Update VendorProduct.js

--- models/VendorProduct.js
+++ models/VendorProduct.js
@@ -1,50 +1,50 @@
-const mongoose = require("mongoose");
+// const mongoose = require("mongoose");
 
-const VendorProductSchema = new mongoose.Schema(
-  {
-    name: { type: String, required: true },
-    description: String,
+// const VendorProductSchema = new mongoose.Schema(
+//   {
+//     name: { type: String, required: true },
+//     description: String,
 
-    restaurantName: { type: String, required: true },
+//     restaurantName: { type: String, required: true },
 
-    oldPrice: Number,
-    newPrice: { type: Number, required: true },
-    quality: String,
-    stock: Number,
+//     oldPrice: Number,
+//     newPrice: { type: Number, required: true },
+//     quality: String,
+//     stock: Number,
 
-    religion: String,
-    productTypes: String,
-    flavors: String,
-    dietPreference: String,
-    nutrition: String,
-    materialTypes: String,
-    ingredients: String,
-    allergenInfo: String,
-    dietaryPreferences: String,
-    cuisine: String,
-    size: String,
+//     religion: String,
+//     productTypes: String,
+//     flavors: String,
+//     dietPreference: String,
+//     nutrition: String,
+//     materialTypes: String,
+//     ingredients: String,
+//     allergenInfo: String,
+//     dietaryPreferences: String,
+//     cuisine: String,
+//     size: String,
 
-    image: String,
-    logo: String,
-    gallery: [String],
+//     image: String,
+//     logo: String,
+//     gallery: [String],
 
-    category: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "VendorCategory",
-      required: true,
-    },
-    subcategory: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "VendorSubCategory",
-    },
+//     category: {
+//       type: mongoose.Schema.Types.ObjectId,
+//       ref: "VendorCategory",
+//       required: true,
+//     },
+//     subcategory: {
+//       type: mongoose.Schema.Types.ObjectId,
+//       ref: "VendorSubCategory",
+//     },
 
-    vendor: {
-      type: mongoose.Schema.Types.ObjectId,
-      ref: "Vendor",
-      required: true,
-    },
-  },
-  { timestamps: true }
-);
+//     vendor: {
+//       type: mongoose.Schema.Types.ObjectId,
+//       ref: "Vendor",
+//       required: true,
+//     },
+//   },
+//   { timestamps: true }
+// );
 
-module.exports = mongoose.model("VendorProduct", VendorProductSchema);
+// module.exports = mongoose.model("VendorProduct", VendorProductSchema);
