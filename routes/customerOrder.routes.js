acc9d2602c29260c419f55ed3c0e310286baa60c
SHEORAN022 <141564857+SHEORAN022@users.noreply.github.com>
2026-01-12 16:27:17 +0530

    Update customerOrder.routes.js

--- routes/customerOrder.routes.js
+++ routes/customerOrder.routes.js
@@ -108,6 +108,39 @@
 
 
 
+// const express = require("express");
+// const router = express.Router();
+
+// const {
+//   createOrder,
+//   getCustomerOrders,
+//   getOrderById,
+//   cancelOrder,
+//   getVendorOrders,
+// } = require("../controllers/customerOrder.controller");
+
+// router.post("/order/create", createOrder);
+
+// router.get("/orders/:customerId", getCustomerOrders);
+
+// router.get("/order/:id", getOrderById);
+
+// router.put("/order/cancel/:id", cancelOrder);
+
+// router.get("/vendor-orders/:vendorId", getVendorOrders);
+
+// module.exports = router;
+
+
+
+
+
+
+
+
+
+
+
 const express = require("express");
 const router = express.Router();
 
